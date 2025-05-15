b034758a6084918064e1411477779f0bffa4a431
Encryptumdev <litehatfndn@gmail.com>
2025-05-15 16:53:14 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,8 @@
-require("@nomiclabs/hardhat-waffle");
-
-module.exports = {
-  solidity: "0.8.17",
-  networks: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: ["YOUR_PRIVATE_KEY_HERE"]
-    }
-  }
-};
+sepolia: {
+  url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+  accounts: ["0xYOUR_PRIVATE_KEY"]
+},
+mainnet: {
+  url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+  accounts: ["0xYOUR_PRIVATE_KEY"]
+}
