fa0372d40578fac4dd5bf23470176980a5d4ce62
Encryptumdev <litehatfndn@gmail.com>
2025-05-15 17:00:06 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,12 @@
-sepolia: {
-  url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
-  accounts: ["0xYOUR_PRIVATE_KEY"]
-},
-mainnet: {
-  url: "https://eth-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
-  accounts: ["0xYOUR_PRIVATE_KEY"]
-}
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/YOUR_ALCHEMY_API_KEY",
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  }
+};
